0dcbd53cb18cc902ebbfa23ff61a109a300859ec
balandina-o <balandina.olga.2021@gmail.com>
2023-11-13 11:06:08 +0500

    Добавление модальных окон

---
+++ src/components/AvalancheModal.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+import { observer } from "mobx-react-lite";
+import "../css_components/OperationPanel.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ChartComponent from "./ChartComponent";
+
+const AvalancheModal = ({ show, onClose }) => {
+  return (
+    <div>
+      <Modal
+        show={show}
+        onHide={() => onClose()}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>ВНИМАНИЕ! Высока вероятность схода лавины</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => onClose()}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => onClose()}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default AvalancheModal;
