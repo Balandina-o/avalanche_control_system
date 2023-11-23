7adec250330c0ccb65b28b2104f8071035d30592
balandina-o <balandina.olga.2021@gmail.com>
2023-11-23 20:22:49 +0500

    del dist

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const isDev = require("electron-is-dev");
 
 // const yourdirectoryorfilenamehere = path.join(
 //   app.getAppPath(),
@@ -18,8 +19,18 @@ const createWindow = () => {
   //mainWindow.loadURL("https://polyakovdmitriy.ru/create-react-app-electron/");
   //mainWindow.loadFile("src/index.js");
   mainWindow.setTitle("Баландина ПИ-426 СРВ"); //на данный момент установлено в title
-  mainWindow.loadURL("http://localhost:3000");
+  //mainWindow.loadURL("http://localhost:3000");
+
   mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../dist/index.html")}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    mainWindow.webContents.openDevTools({ mode: "detach" });
+  }
 
   // const startUrl =
   //   process.env.ELECTRON_START_URL ||
